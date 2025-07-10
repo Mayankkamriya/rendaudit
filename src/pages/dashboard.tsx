ca512c5d22ca6b23bdd9d86a6515dfe3d7885708
mayankkamriya <kamriyamayank45@gmail.com>
2025-07-10 13:37:18 +0530

    problem fix

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -11,7 +11,7 @@ import {
   Users,
   DollarSign
 } from 'lucide-react';
-import { Listing, AuditLog } from '../types';
+import { Listing, AuditLog } from '../../types';
 
 interface DashboardProps {
   stats: {
